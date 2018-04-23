93dd2b80884523f730f4cef665dd80e1a7d6e157
Brady Dufresne <bjdufre1@svsu.edu>
2018-04-23 18:48:52 -0400

    inital

--- src/Translator.java
+++ src/Translator.java
@@ -1,7 +1,16 @@
+import java.io.*;
+
 public class Translator {
 
-    public static void main(String[] args) {
+    public static BufferedWriter bw;
+    public static BufferedReader br;
 
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new FileReader("input.txt"));
+        bw = new BufferedWriter(new FileWriter("output.txt"));
+        while (br.readLine() != null){
+            System.out.println("line");
+        }
     }
 
 }
